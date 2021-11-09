56052955e8c760a730b9af1fca18e0ec82f96bee
Suhani Desale <deslesuhani02@gmail.com>
2021-11-09 23:53:59 +0530

    The Iterator Design Pattern

---
+++ Iterator/src/refactoring_guru/iterator/example/iterators/LinkedInIterator.java
@@ -0,0 +1,8 @@
+package refactoring_guru.iterator.example.iterators;
+
+import refactoring_guru.iterator.example.social_networks.LinkedIn;
+
+public class LinkedInIterator {
+    public LinkedInIterator(LinkedIn linkedIn, String friends, String profileEmail) {
+    }
+}
